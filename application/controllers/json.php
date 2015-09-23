1d2b0da8a47999d29455a216391d5046a0bb95e4
Pooja <pthakare33@gmail.com>
2015-09-23 17:19:19 +0530

    newarrival done

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -1416,6 +1416,8 @@ echo $filepath;
      public function getexclusiveandnewarrival()
     {
         $id = $this->input->get_post("id");
+                  
+          $userid=$this->session->userdata("id");
         $getproductids=$this->db->query("SELECT * FROM `newarrival` WHERE `type` IN ($id,3)")->result();
           $productids="(";
             foreach($getproductids as $key=>$value)
@@ -1504,6 +1506,12 @@ echo $filepath;
         $elements[13]->sort = "1";
         $elements[13]->header = "typeid";
         $elements[13]->alias = "typeid";
+         
+        $elements[14] = new stdClass();
+        $elements[14]->field = "`userwishlist`.`user`";
+        $elements[14]->sort = "1";
+        $elements[14]->header = "isfavid";
+        $elements[14]->alias = "isfavid";
         $search = $this->input->get_post("search");
         $pageno = $this->input->get_post("pageno");
         $orderby = $this->input->get_post("orderby");
@@ -1512,7 +1520,7 @@ echo $filepath;
         if ($maxrow == "") {
             $maxrow = 20;
         }
-        $data["message"] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, "FROM `product` LEFT OUTER JOIN `newarrival` ON `newarrival`.`product`=`product`.`id` LEFT OUTER JOIN `productimage` as `image2` ON `image2`.`product`=`product`.`id` AND `image2`.`order`=0 LEFT OUTER JOIN `productimage` as `image1` ON `image1`.`product`=`product`.`id` AND `image1`.`order`=1", "WHERE `product`.`visibility`=1 AND `product`.`status`=1 AND  `product`.`id` IN $productids", ' GROUP BY `product`.`id` ');
+        $data["message"] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, "FROM `product` LEFT OUTER JOIN `newarrival` ON `newarrival`.`product`=`product`.`id` LEFT OUTER JOIN `userwishlist` ON `userwishlist`.`product`=`product`.`id` AND `userwishlist`.`user`='$userid' LEFT OUTER JOIN `productimage` as `image2` ON `image2`.`product`=`product`.`id` AND `image2`.`order`=0 LEFT OUTER JOIN `productimage` as `image1` ON `image1`.`product`=`product`.`id` AND `image1`.`order`=1", "WHERE `product`.`visibility`=1 AND `product`.`status`=1 AND  `product`.`id` IN $productids", ' GROUP BY `product`.`id` ');
         $this->load->view("json", $data);
     }
 
