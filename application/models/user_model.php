ade21a9b547177d44079b0606cf4330a22709958
Pooja <pooja.wohlig@gmail.com>
2015-11-25 14:30:37 +0530

    tk all user data

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -743,7 +743,8 @@ $timestamp=new DateTime();
 
 $this->db->where('id', $id);
 $val=$this->db->update('user', $data); 
-        return $val;
+        $userdata=$this->user_model->getuserdetails($id);
+        return $userdata;
     }
       function getuserdetails($id){
     $query=$this->db->query("SELECT `id`, `firstname`, `lastname`, `email`, `phone`,`billingaddress`, `billingcity`, `billingstate`, `billingcountry`, `billingpincode` FROM `user` WHERE `id`='$id'")->row();
