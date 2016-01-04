6ddf6d0b17095389e8449ec8db1d1ba9dd5b2f8e
Pooja <pooja.wohlig@gmail.com>
2016-01-04 20:09:48 +0530

    chanegs

--- application/views/backend/includes/dashboard.php
+++ application/views/backend/includes/dashboard.php
@@ -12,6 +12,7 @@
     </section>
 </div>
 
+<!--
 <div class="col-lg-3 col-sm-3">
     <section class="panel">
         <div class="symbol terques">
@@ -25,6 +26,7 @@
 
     </section>
 </div>
+-->
 
 <form method="post" action="<?php echo site_url('site/getDate');?>">
     <div class="row">
@@ -36,65 +38,6 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="form-group">
-            <label class="col-sm-2 control-label" for="normal-field"><strong>Select Month</strong></label>
-            <div class="col-sm-4">
-                <select id='gMonth2' name="month">
-                    <option value=''>--Select Month--</option>
-                    <option selected value='1'>Janaury</option>
-                    <option value='2'>February</option>
-                    <option value='3'>March</option>
-                    <option value='4'>April</option>
-                    <option value='5'>May</option>
-                    <option value='6'>June</option>
-                    <option value='7'>July</option>
-                    <option value='8'>August</option>
-                    <option value='9'>September</option>
-                    <option value='10'>October</option>
-                    <option value='11'>November</option>
-                    <option value='12'>December</option>
-                </select>
-            </div>
-        </div>
-
-
-
-        <div class="form-group">
-            <label class="col-sm-2 control-label" for="normal-field"><strong>Select Year</strong></label>
-            <div class="col-sm-4">
-                <!--                <input type="date" id="normal-field" class="form-control" name="orderdate" value='<?php echo set_value(' orderdate ',$orderdate);?>'>-->
-                <select id="birthyear" name="year">
-                    <option value="2015">2015</option>
-                    <option value="2016">2016</option>
-                    <option value="2017">2017</option>
-                    <option value="2018">2018</option>
-                    <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                    <option value="2026">2026</option>
-                    <option value="2027">2027</option>
-                    <option value="2028">2028</option>
-                    <option value="2029">2029</option>
-                    <option value="2030">2030</option>
-                    <option value="2031">2031</option>
-                    <option value="2032">2032</option>
-                    <option value="2033">2033</option>
-                    <option value="2034">2034</option>
-                    <option value="2035">2035</option>
-                    <option value="2036">2036</option>
-                    <option value="2037">2037</option>
-                    <option value="2038">2038</option>
-                    <option value="2039">2039</option>
-                    <option value="2040">2040</option>
-                </select>
-            </div>
-        </div>
-    </div>
     <button type="submit" class="btn btn-info">Submit</button>
 </form>
 
