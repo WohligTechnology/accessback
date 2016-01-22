1d2ba951f9210ceb8ead140fcfe307f08edca6cd
mahesh maurya <mahesh@wohlig.com>
2016-01-22 14:04:41 +0530

    chanegs for order

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -1411,7 +1411,7 @@ echo $filepath;
             $orderby = "id";
             $orderorder = "ASC";
         }
-        $data["message"] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, "FROM `orderitems` INNER JOIN `order` ON `order`.`id`=`orderitems`.`order` LEFT OUTER JOIN `orderstatus` ON `orderstatus`.`id`=`order`.`orderstatus`","WHERE `order`.`user`='$userid'","GROUP BY `order`.`id`");
+        $data["message"] = $this->chintantable->query($pageno, $maxrow, $orderby, $orderorder, $search, $elements, "FROM `orderitems` INNER JOIN `order` ON `order`.`id`=`orderitems`.`order` LEFT OUTER JOIN `orderstatus` ON `orderstatus`.`id`=`order`.`orderstatus`","WHERE `order`.`user`='$userid' AND `orderstatus` NOT IN(1,5)","GROUP BY `order`.`id`");
         
 //        multiple products
         
