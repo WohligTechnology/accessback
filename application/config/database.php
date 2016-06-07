d07205a12be3a84571d3725b6c4f9e0303c918fe
Pooja <pthakare33@gmail.com>
2016-06-07 11:21:15 +0530

    db for server

--- application/config/database.php
+++ application/config/database.php
@@ -69,25 +69,9 @@ if($myserver!="localhost" && $ip != "192.168" )
   //  $db['default']['autoinit'] = TRUE;
   //  $db['default']['stricton'] = FALSE;
 
-    // $db['default']['username'] = 'root';
-    // $db['default']['password'] = 'YT4C-Xci+SxZAR';
-    // $db['default']['database'] = 'acceskq2_fynx';
-    // $db['default']['dbdriver'] = 'mysql';
-    // $db['default']['dbprefix'] = '';
-    // $db['default']['pconnect'] = TRUE;
-    // $db['default']['db_debug'] = TRUE;
-    // $db['default']['cache_on'] = FALSE;
-    // $db['default']['cachedir'] = '';
-    // $db['default']['char_set'] = 'utf8';
-    // $db['default']['dbcollat'] = 'utf8_general_ci';
-    // $db['default']['swap_pre'] = '';
-    // $db['default']['autoinit'] = TRUE;
-    // $db['default']['stricton'] = FALSE;
-
-    $db['default']['hostname'] = 'localhost';
     $db['default']['username'] = 'root';
-    $db['default']['password'] = '';
-    $db['default']['database'] = 'access';
+    $db['default']['password'] = 'YT4C-Xci+SxZAR';
+    $db['default']['database'] = 'acceskq2_fynx';
     $db['default']['dbdriver'] = 'mysql';
     $db['default']['dbprefix'] = '';
     $db['default']['pconnect'] = TRUE;
@@ -99,6 +83,22 @@ if($myserver!="localhost" && $ip != "192.168" )
     $db['default']['swap_pre'] = '';
     $db['default']['autoinit'] = TRUE;
     $db['default']['stricton'] = FALSE;
+
+    // $db['default']['hostname'] = 'localhost';
+    // $db['default']['username'] = 'root';
+    // $db['default']['password'] = '';
+    // $db['default']['database'] = 'access';
+    // $db['default']['dbdriver'] = 'mysql';
+    // $db['default']['dbprefix'] = '';
+    // $db['default']['pconnect'] = TRUE;
+    // $db['default']['db_debug'] = TRUE;
+    // $db['default']['cache_on'] = FALSE;
+    // $db['default']['cachedir'] = '';
+    // $db['default']['char_set'] = 'utf8';
+    // $db['default']['dbcollat'] = 'utf8_general_ci';
+    // $db['default']['swap_pre'] = '';
+    // $db['default']['autoinit'] = TRUE;
+    // $db['default']['stricton'] = FALSE;
 }
 else
 {
