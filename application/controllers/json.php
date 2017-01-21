819e10ddc3bc563e8cca545ff9bc794903b0d0b1
Chintan Shah <chintan@wohlig.com>
2017-01-21 17:16:42 +0530

    DESC

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -1142,7 +1142,7 @@ echo $filepath;
         INNER JOIN `producttype` ON `producttype`.`product`=`product`.`id`
         LEFT OUTER JOIN `userwishlist` ON `userwishlist`.`product`=`product`.`id` AND `userwishlist`.`user`='$userid'
         LEFT OUTER JOIN `productimage` as `image2` ON `image2`.`product`=`product`.`id` AND `image2`.`order`=1
-        LEFT OUTER JOIN `productimage` as `image1` ON `image1`.`product`=`product`.`id` AND `image1`.`order`=0", "WHERE `product`.`status`=1 $where 1 " , ' GROUP BY `product`.`id` ');
+        LEFT OUTER JOIN `productimage` as `image1` ON `image1`.`product`=`product`.`id` AND `image1`.`order`=0", "WHERE `product`.`status`=1 $where 1 " , ' GROUP BY `product`.`id` ',"","ORDER BY `id` DESC");
 
         if($this->input->get("offline") != "1")
         {
